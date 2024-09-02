85c094d9e9c6281e4df07da58e62f90cdb65e703
riff <89750544+removetooth@users.noreply.github.com>
2024-09-01 19:31:53 -0500

    navmesh

--- cult-game-version-0.1/scripts/enemy.gd
+++ cult-game-version-0.1/scripts/enemy.gd
@@ -6,8 +6,12 @@ var player_pos
 var target_pos
 @onready var player = get_parent().get_node("player")
 
+@export var navAgent:NavigationAgent2D;
+
 func _process(delta: float) -> void:
-	var direction = ( player.global_position - self.global_position ).normalized()
+	navAgent.target_position = player.global_position;
+	var direction = global_position.direction_to(navAgent.get_next_path_position());
+	#( player.global_position - self.global_position ).normalized()
 	
 	velocity.x = move_toward(velocity.x / speed, direction.x, accel * delta) * speed
 	velocity.y = move_toward(velocity.y / speed, direction.y, accel * delta) * speed
