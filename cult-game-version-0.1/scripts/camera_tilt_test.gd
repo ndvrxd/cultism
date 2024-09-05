4c9ab692e58bd750d71092fdb3c9cefd14b0e0c2
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-04 21:31:45 -0500

    marginally better camera zoom

--- cult-game-version-0.1/scripts/camera_tilt_test.gd
+++ cult-game-version-0.1/scripts/camera_tilt_test.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var rate:float = 0.1
+@export var rate:float = 0.5
 const refreshRate:float = 2
 var refreshTimer:float = refreshRate - 0.1
 var sprites;
@@ -10,10 +10,12 @@ func _process(delta: float) -> void:
 	if NetManager.IsDedicated(): return
 	if Input.is_action_pressed("CameraTiltUp"):
 		zoom.y += rate * delta
+		zoom.y = clampf(zoom.y, 0.4, 1)
 		for s:Node2D in sprites:
 			s.scale.y = 1 / zoom.y;
 	if Input.is_action_pressed("CameraTiltDown"):
 		zoom.y -= rate * delta
+		zoom.y = clampf(zoom.y, 0.4, 1)
 		for s:Node2D in sprites:
 			s.scale.y = 1 / zoom.y;
 	
