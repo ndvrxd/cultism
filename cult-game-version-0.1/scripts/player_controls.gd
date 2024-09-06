c9787e947cc88d6f7421c04c500fe032141d96d7
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-06 18:43:44 -0500

    keyboard only control scheme
    this fucks

--- cult-game-version-0.1/scripts/player_controls.gd
+++ cult-game-version-0.1/scripts/player_controls.gd
@@ -1,17 +1,29 @@
 extends EntityController
 
 var direction:Vector2
+var mouseLookMode = true
+var lastMousePos:Vector2 = Vector2.ZERO
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down");
 	ent.moveIntent = direction
 	
-	ent.lookDirection = ent.shoulderPoint.global_position.direction_to(get_global_mouse_position())
+	var cameraStickDir = Input.get_vector("LookLeft", "LookRight", "LookUp", "LookDown").normalized();
+	if mouseLookMode:
+		ent.lookDirection = ent.shoulderPoint.global_position.direction_to(get_global_mouse_position())
+		if cameraStickDir != Vector2.ZERO: mouseLookMode = false
+	else:
+		ent.lookDirection = ent.lookDirection.lerp(cameraStickDir, 20*delta)
+		if Input.get_last_mouse_velocity().length() > 0: mouseLookMode = true
 
 func _process(_delta):
 	if Input.is_action_just_pressed("PrimaryAttack"):
 		ent.primaryFire.rpc(get_global_mouse_position())
 	if Input.is_action_just_pressed("SecondaryAttack"):
 		ent.secondaryFire.rpc(get_global_mouse_position())
+	if Input.is_action_just_released("PrimaryAttack"):
+		ent.primaryFireReleased.rpc(get_global_mouse_position())
+	if Input.is_action_just_released("SecondaryAttack"):
+		ent.secondaryFireReleased.rpc(get_global_mouse_position())
 	if Input.is_action_just_pressed("SpecialAbility"):
 		ent.activeAbility.rpc(get_global_mouse_position())
