41ccef2f4cd6d433201325a7f0a2fd5c670c8c9d
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-06 20:24:19 -0500

    THE COOLER SWORD

--- cult-game-version-0.1/scripts/player.gd
+++ cult-game-version-0.1/scripts/player.gd
@@ -1,8 +1,13 @@
 extends Entity
 
 var swordShape:CircleShape2D = CircleShape2D.new()
-var swordRange = 100
-var swordDamage = 34
+@export var swordWoosh:GPUParticles2D;
+@export var swordRange = 100
+@export var swordDamage = 34
+
+var swingTimer = 0;
+@export var swingDelay = 0.65;
+@export var maxHits = 3;
 
 var swordHitFx:PackedScene = preload("res://objects/vfx/sword_hit.tscn")
 
@@ -11,15 +16,27 @@ func _ready():
 	swordShape.radius = 50;
 	hit_landed.connect(onHit)
 
+func _process(delta):
+	super._process(delta)
+	if swingTimer > 0: swingTimer -= delta
+
 @rpc("any_peer", "call_local", "reliable")
 func primaryFire(target:Vector2) -> void:
 	super.primaryFire(target)
 	if multiplayer.get_remote_sender_id() != multiplayer.get_unique_id(): return
 	# everything beyond this point happens clientside
-	var ents = shapeCastFromShoulder(lookDirection*swordRange, swordShape)
-	for e:Entity in ents:
-		if team != e.team:
-			e.changeHealth.rpc(e.health, -swordDamage, get_path())
+	
+	if swingTimer <= 0:
+		var ents = shapeCastFromShoulder(lookDirection*swordRange, swordShape)
+		var hits = 0
+		for e:Entity in ents:
+			if team != e.team:
+				e.changeHealth.rpc(e.health, -swordDamage, get_path())
+				hits += 1
+			if hits >= maxHits:
+				break 
+		swordWoosh.restart()
+		swingTimer = swingDelay
 			
 func onHit(pos:Vector2, normal:Vector2):
 	var temp = swordHitFx.instantiate()
