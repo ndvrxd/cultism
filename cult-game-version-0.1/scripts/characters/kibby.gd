920192d83e3f97a3dc5f10253699f9d4f57e9d37
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-10 14:33:31 -0500

    kibby art & attack

--- cult-game-version-0.1/scripts/characters/kibby.gd
+++ cult-game-version-0.1/scripts/characters/kibby.gd
@@ -1,7 +1,46 @@
 extends Entity
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+var swordShape:CircleShape2D = CircleShape2D.new()
+@export var guitarRange = 100
+@export var guitarDamage = 20
+
+@export var maxHits = 8;
+
+var swordHitFx:PackedScene = preload("res://vfx/objects/sword_hit.tscn")
+
+func _ready():
 	super._ready()
-	stat_speed = Stat.fromBase(500);
-	stat_maxHp = Stat.fromBase(120);
+	stat_speed = Stat.fromBase(300)
+	stat_maxHp = Stat.fromBase(120)
+	swordShape.radius = 100;
+	hit_landed.connect(onHit)
+	primaryCD = 0.9
+	activeCD = 25
+
+func _process(delta):
+	$Node2D/Sprite2D.flip_h = lookDirection.x < 0
+	super._process(delta)
+
+func primaryFireAction():
+	$shoulder/swordwoosh.restart()
+	$shoulder/swordwoosh.scale.y = -$shoulder/swordwoosh.scale.y
+	
+func primaryFireActionAuthority():
+	var ents = shapeCastFromShoulder(lookDirection*guitarRange, swordShape, false)
+	var hits = 0
+	for e:Entity in ents:
+		triggerHitEffectsRpc.rpc(e.shoulderPoint.global_position)
+		if team != e.team:
+			e.changeHealth.rpc(e.health, -guitarDamage, get_path())
+			hits += 1
+		if hits >= maxHits:
+			break
+
+func activeAbilityAction():
+	pass #wololo aoe here
+	
+func onHit(pos:Vector2, normal:Vector2):
+	var temp = swordHitFx.instantiate()
+	temp.emitting = true
+	temp.global_position = pos
+	get_parent().add_child(temp)
--- cult-game-version-0.1/scripts/menus/connectScreen.gd
+++ cult-game-version-0.1/scripts/menus/connectScreen.gd
@@ -6,19 +6,28 @@ extends Node
 @export var errorMsg:Label;
 @export var charBodySel:ItemList;
 
+@export var path:String = "res://objects/characters/{}.tscn"
+
 func _ready():
 	NetManager.connection_refused.connect(emsgCallback)
 	charBodySel.select(0)
 	
 func onJoinClicked():
 	NetManager.playerinfo_local["name"] = nameBox.text;
-	NetManager.playerinfo_local["charbody"] = charBodySel.get_item_text(charBodySel.get_selected_items()[0]);
+	NetManager.playerinfo_local["charbody"] = path.format(
+		[charBodySel.get_item_text(charBodySel.get_selected_items()[0])],
+		"{}"
+		);
 	#NetManager.playerinfo_local["color"] = color.color;
 	NetManager.join_game(addressBox.text, portBox.text.to_int());
 	
 func onCreateClicked():
 	NetManager.playerinfo_local["name"] = nameBox.text;
-	NetManager.playerinfo_local["charbody"] = charBodySel.get_item_text(charBodySel.get_selected_items()[0]);
+	NetManager.playerinfo_local["charbody"] = path.format(
+		[charBodySel.get_item_text(charBodySel.get_selected_items()[0])],
+		"{}"
+		);
+	print(NetManager.playerinfo_local["charbody"])
 	#NetManager.playerinfo_local["color"] = color.color;
 	NetManager.start_listen_server("localhost", portBox.text.to_int());
 
