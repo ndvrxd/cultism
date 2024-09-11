f46446e8cd995c3acf63d93f302ec62994302c64
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-11 17:17:19 -0500

    kibby sfx draft

--- cult-game-version-0.1/scripts/characters/kibby.gd
+++ cult-game-version-0.1/scripts/characters/kibby.gd
@@ -8,6 +8,8 @@ var aoeShape:CircleShape2D = CircleShape2D.new()
 
 @export var maxHits = 8;
 
+@onready var snd_lightning:AudioStreamMP3 = load("res://sound/fx/thunder.mp3")
+
 var lightningMeter = 0;
 
 var swordHitFx:PackedScene = preload("res://vfx/objects/sword_hit.tscn")
@@ -25,7 +27,6 @@ func _ready():
 	primaryCD = 0.9
 	activeCD = 25
 	$AoeHitboxTick.timeout.connect(starstruckTick)
-	$AoeDuration.timeout.connect(starstruckExpire)
 
 func _process(delta):
 	$Node2D.scale.x = sign(lookDirection.x)
@@ -37,8 +38,10 @@ func syncLightningMeter(value:float) -> void:
 	lightningMeter = value
 
 func primaryFireAction():
+	$snd_whiff.play()
 	$shoulder/swordwoosh.restart()
 	$shoulder/swordwoosh.scale.y = -$shoulder/swordwoosh.scale.y
+	await get_tree().create_timer(0.15).timeout
 	var ents = shapeCastFromShoulder(lookDirection*guitarRange, meleeShape, false)
 	var hits = 0
 	for e:Entity in ents:
@@ -49,9 +52,12 @@ func primaryFireAction():
 				triggerHitEffectsRpc.rpc(e.shoulderPoint.global_position)
 				syncLightningMeter.rpc(lightningMeter)
 			if lightningMeter >= 1 and not $Node2D/m2readyloop.emitting:
+				$snd_secondarycharged.play()
 				$Node2D/m2readyloop.emitting = true
 				$Node2D/m2readycue.restart()
 			hits += 1
+		if hits > 0:
+			$snd_guitarhit.play()
 		if hits >= maxHits:
 			break
 
@@ -64,6 +70,11 @@ func secondaryFireAction():
 	temp.lifetime = 0.2
 	temp.fixed_fps = 60
 	get_tree().current_scene.add_child(temp)
+	var sfx:AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+	sfx.stream = snd_lightning
+	sfx.global_position = aimPosition
+	get_tree().current_scene.add_child(sfx)
+	sfx.play()
 	lightningMeter = 0
 	$Node2D/m2readyloop.emitting = false
 	if is_multiplayer_authority():
@@ -74,13 +85,19 @@ func secondaryFireAction():
 		temp.team = team
 		temp.inflictor = get_path()
 		get_tree().current_scene.add_child(temp)
+	await get_tree().create_timer(5).timeout
+	sfx.queue_free()
 
 func activeAbilityAction():
 	$AoeBurst.restart()
-	$AoeDuration.start()
 	$AoeHitboxTick.start()
+	$snd_starstruck.play()
 	stat_speed.modifyMultFlat(-0.99)
 	velocity = Vector2.ZERO
+	await get_tree().create_timer(0.65).timeout
+	$AoeHitboxTick.stop()
+	await get_tree().create_timer(1.45).timeout
+	stat_speed.modifyMultFlat(0.99)
 
 func starstruckTick():
 	var ents = shapeCastFromShoulder(Vector2.ZERO, aoeShape, false)
@@ -91,10 +108,6 @@ func starstruckTick():
 			get_tree().current_scene.add_child(temp)
 			if is_multiplayer_authority():
 				e.changeTeam.rpc(team)
-
-func starstruckExpire():
-	$AoeHitboxTick.stop()
-	stat_speed.modifyMultFlat(0.99)
 	
 func onHit(pos:Vector2, normal:Vector2):
 	var temp = swordHitFx.instantiate()
