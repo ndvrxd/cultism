ec1824517e2bced2666807c693bbfa7b3d065ed1
removetooth <89750544+removetooth@users.noreply.github.com>
2024-09-19 21:15:42 -0500

    music & audio mixing test

--- cult-game-version-0.1/scripts/characters/enemy.gd
+++ cult-game-version-0.1/scripts/characters/enemy.gd
@@ -12,11 +12,13 @@ func _process(delta:float) -> void:
 	if lockInDirection != Vector2.ZERO:
 		lookDirection = lockInDirection
 	super._process(delta)
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
 	$upright_anchor.scale.x = 1 if lookDirection.x > 0 else -1
 	$upright_anchor/eyes.position = Vector2(randf_range(-1.5, 1.5), 3+randf_range(-1.5, 1.5))
 
 func primaryFireAction():
-	$telegraph.start()
 	$upright_anchor/tell.restart()
 	lockInDirection = lookDirection
 	stat_speed.modifyMultFlat(-0.99)
