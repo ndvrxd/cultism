525104783cbd3cc1eacc4a279ca2456b1eb3e048
littlegrosslittleguy <115913973+littlegrosslittleguy@users.noreply.github.com>
2024-10-26 03:16:00 -0400

    Refined item select menu.
    made window responsive, added fading animations, attached it to the main scene better. confirm button disabled without item selected. now only needs to have the world paused when open, but that disables the list for some reason...

--- cult-game-version-0.1/scripts/menus/item_menu.gd
+++ cult-game-version-0.1/scripts/menus/item_menu.gd
@@ -6,7 +6,22 @@ var my_entity: Entity
 var selected_item: String
 
 
-# todo: add a close on e function
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+	visible = false
+
+func _process(delta: float) -> void:
+	# can't confirm without something selected
+	if not %ItemList.is_anything_selected():
+		%ConfirmButton.disabled = true
+	else:
+		%ConfirmButton.disabled = false
+	
+	# TODO: fix this
+	#if Input.is_action_just_pressed("Interact") and visible:
+		#visible = false
+
+
 func setup(entity: Entity) -> void:
 	my_entity = entity
 
@@ -17,4 +32,23 @@ func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_i
 
 func _on_confirm_button_pressed() -> void:
 	my_entity.addItem(load(selected_item))
-	queue_free()
+	hide_me()
+
+
+func show_me() -> void:
+	visible = true
+	%ItemList.deselect_all()
+	%AnimationPlayer.play("blur")
+	# this SHOULD work, but for some reason
+	# the items arent selectable
+	#if multiplayer.is_server() and NetManager.players.size() < 2:
+		#get_tree().paused = true
+
+
+func hide_me() -> void:
+	%AnimationPlayer.play_backwards("blur")
+	await get_tree().create_timer(0.3).timeout
+	visible = false
+	
+	#if get_tree().paused:
+		#get_tree().paused = false
