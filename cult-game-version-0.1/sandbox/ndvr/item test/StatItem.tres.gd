765b7974e3b74c51657ca45b309cce5ada6de824
ndvrxd <ndvrxd@gmail.com>
2024-10-29 02:44:01 -0400

    Added items!
    -Added 5 new items changing the respective stats to players that pick them up
    -Added ndvr sandbox
    
    to test items go to ndvr sandbox and fuck around in fraggot.tscn (my test world)

---
+++ cult-game-version-0.1/sandbox/ndvr/item test/StatItem.tres.gd
@@ -0,0 +1,24 @@
+extends PassiveItem
+
+
+@export var SpeedModify:float = 0
+@export var LuckModify:float = 0
+@export var HealthModify:float = 0
+@export var DamageModify:float = 0
+@export var RegenModify:float = 0
+@export var AggroModify:float = 0
+
+
+func _on_stack_count_changed(delta: int) -> void:
+	#commenting all this cus i still wanna mess around with it l8r! -ndvr :3
+	#var temp:String = ""
+	#for i in range(stackCount):
+	#	temp += "+10 Speed\n"
+	#$Label.text = temp
+	ent.stat_speed.modifyMultFlat(SpeedModify * delta)
+	ent.stat_speed.modifyMultFlat(LuckModify * delta) #HEY! STUPID! MAKE THIS CHANGE LUCK WHEN ITS ADDED TO ENTITY.GD
+	ent.stat_maxHp.modifyMultFlat(HealthModify * delta)
+	ent.stat_baseDamage.modifyMultFlat(DamageModify * delta)
+	ent.stat_regen.modifyMultFlat(RegenModify * delta)
+	ent.stat_aggroNoise.modifyMultFlat(AggroModify * delta)
+	
