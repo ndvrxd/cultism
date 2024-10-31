42e2903796a9b44adf3b3c9f315ec38eaa5172aa
removetooth <89750544+removetooth@users.noreply.github.com>
2024-10-31 12:38:07 -0500

    generalize aim assist for non-mouse aim

--- cult-game-version-0.1/scripts/ability-generic/pistol_generic.gd
+++ cult-game-version-0.1/scripts/ability-generic/pistol_generic.gd
@@ -11,28 +11,12 @@ extends Ability
 ## @experimental: Damage calculations will very likely change drastically in the future.
 ## How much to multiply [member Entity.stat_baseDamage] by in damage calculations.
 @export var gunDamageCoeff:float = 2
-## An [Area2D], meant to be a cone protruding from the shoulder. Should face right
-## by default, and be placed on the world origin. Can be left unset for no
-## aim assist.
-@export var aimAssistZone:Area2D
-
-func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
-	if aimAssistZone:
-		alignNodeWithShoulder(aimAssistZone)
 
 func fireAction():
-	var aimAssist:Vector2 = ent.lookDirection
-	if aimAssistZone:
-		var sucker:Entity = ent.prioritize(aimAssistZone.get_overlapping_areas())
-		if sucker: aimAssist = ent.shoulderPoint.global_position.direction_to(
-				sucker.shoulderPoint.global_position
-				)
-	
-	var hit = ent.lineCastFromShoulder(aimAssist, gunRange)
+	var hit = ent.lineCastFromShoulder(ent.lookDirection, gunRange)
 	var tr = hitscanTracer.instantiate()
 	tr.global_position = ent.shoulderPoint.global_position
-	tr.setDirection(aimAssist)
+	tr.setDirection(ent.lookDirection)
 	tr.setRange(ent.shoulderPoint.global_position.distance_to(hit["pos"]), 70)
 	get_tree().current_scene.add_child(tr)
 	if is_multiplayer_authority():
--- cult-game-version-0.1/scripts/ai/player_controls.gd
+++ cult-game-version-0.1/scripts/ai/player_controls.gd
@@ -9,7 +9,7 @@ var mouseLookMode = true
 @onready var ui_hb_effigy:TextureProgressBar = $"HUD STUFF/objective_health"
 var vignetteTween:Tween
 
-const spectatorBody:String = "res://objects/characters/spectator.tscn"
+const SPECTATOR_BODY_PATH:String = "res://objects/characters/spectator.tscn"
 var statusEffectIcon = preload("res://objects/statusEffectIcon.tscn")
 
 var controllerAimLength:float = 0
@@ -17,38 +17,50 @@ var controllerAimMinimum:float = 100
 var controllerAimMaximum:float = 600
 var controllerAimSpeed:float = 400
 var lastStickDir:Vector2 = Vector2.ZERO
+var trueLookDirection:Vector2 = Vector2.ZERO
 
 # for time elapsed
 var seconds: float = 0.0
 var minutes: int = 0
 
+const ABILITY_ACTION_MAP:Array[String] = [
+	"PrimaryAttack",
+	"SecondaryAttack",
+	"SpecialAbility",
+	"dedicated meow button"
+]
+@onready var ABILITY_ICON_MAP:Array[Control] = [
+	$"HUD STUFF/primaryIcon",
+	$"HUD STUFF/secondaryIcon",
+	$"HUD STUFF/specialIcon",
+	$"HUD STUFF/meowButton"
+]
+
 func _ready():
 	super._ready()
+	
+	$"HUD STUFF/vignette".modulate = Color(Color.RED, 0)
+	$"HUD STUFF/itemdialog".visible = false
+	
 	ui_hb_over.tint_progress = ent.healthBarColor
 	ent.damage_taken.connect(damageVignette)
-	$"HUD STUFF/vignette".modulate = Color(Color.RED, 0)
 	ent.killed.connect(spectate)
 	ent.effect_added.connect(_on_status_effect_added)
 	ent.item_acquired.connect(_on_item_acquired)
-	$"HUD STUFF/itemdialog".visible = false
 	Chatbox.inst.set_username(ent.entityName)
-	if ent.abilities.size() > 0 and is_instance_valid(ent.abilities[0]): 
-		$"HUD STUFF/primaryIcon".set_ability(ent.abilities[0])
-	if ent.abilities.size() > 1 and is_instance_valid(ent.abilities[1]): 
-		$"HUD STUFF/secondaryIcon".set_ability(ent.abilities[1])
-	if ent.abilities.size() > 2 and is_instance_valid(ent.abilities[2]): 
-		$"HUD STUFF/specialIcon".set_ability(ent.abilities[2])
-	if ent.abilities.size() > 3 and is_instance_valid(ent.abilities[3]): 
-		$"HUD STUFF/meowButton".set_ability(ent.abilities[3])
+	
+	for i in range(ABILITY_ICON_MAP.size()):
+		if ent.abilities.size() > i and is_instance_valid(ent.abilities[i]): 
+			ABILITY_ICON_MAP[i].set_ability(ent.abilities[i])
 
-func spectate(killedBy:Entity):
+func spectate(killedBy:Entity): # This will need to change to allow respawning
 	var ename:String = ent.name
 	Chatbox.inst.print_chat.rpc("* " + ename + " was killed by " + killedBy.entityName + ".")
 	var epos:Vector2 = ent.global_position
 	reparent(get_tree().current_scene)
 	global_position = ent.global_position
 	await get_tree().create_timer(0.1).timeout
-	Entity.spawn(spectatorBody, epos, '', ename)
+	Entity.spawn(SPECTATOR_BODY_PATH, epos, '', ename)
 	await get_tree().create_timer(0.1).timeout
 	queue_free()
 	
@@ -62,25 +74,68 @@ func _physics_process(delta: float) -> void:
 	ent.moveIntent = direction if !Chatbox.isFocused else Vector2.ZERO
 	
 	var cameraStickDir = Input.get_vector("LookLeft", "LookRight", "LookUp", "LookDown").normalized();
-	if mouseLookMode:
-		ent.lookDirection = ent.shoulderPoint.global_position.direction_to(get_global_mouse_position())
-		ent.aimPosition = get_global_mouse_position()
-		if cameraStickDir != Vector2.ZERO: mouseLookMode = false
-	else:
+	
+	if mouseLookMode: # if aiming with the mouse
+		var mp:Vector2 = get_global_mouse_position()
+		ent.lookDirection = ent.shoulderPoint.global_position.direction_to(mp)
+		ent.aimPosition = mp
+		if cameraStickDir != Vector2.ZERO:
+			# switch off mouse aim if aiming with keyboard / controller
+			$assist/shape.disabled = false
+			mouseLookMode = false
+		
+	else: # if aiming with controller or keyboard
+		
+		# look direction smoothing
+		# i dont remember what the thinking behind the checks is here,
+		# all i know is that it works
+		if cameraStickDir.distance_to(-trueLookDirection) > 1:
+			# if it can be reasonably lerped to, lerp to the intended look direction
+			trueLookDirection = trueLookDirection.lerp(cameraStickDir, 20*delta)
+		elif cameraStickDir.length() > 0:
+			# if the player is trying to snap around, there's no time to wait
+			# just set their look direction to the intended aim immediately
+			trueLookDirection = cameraStickDir
+			controllerAimLength = 0
+		
+		# control the "cursor" position on screen based on how long
+		# a specific direction is held & rein in based on delta in look direction
+		# used for "freehand" abilities that are aimed with the mouse, such as
+		# teleportation, orbital strike, or whatever
 		if cameraStickDir.length() > 0:
+			# increase or decrease the distance the cursor is at while
+			# keeping it within a set range
+			# the math here is stupid and unreadable... sorry!
 			controllerAimLength += controllerAimSpeed * cameraStickDir.length() * delta
 			controllerAimLength = clampf(controllerAimLength /
 					(1 + lastStickDir.distance_to(cameraStickDir) * 0.4),
 					0, controllerAimMaximum)
-		ent.aimPosition = ent.global_position + ent.lookDirection * \
+		ent.aimPosition = ent.global_position + trueLookDirection * \
 				(controllerAimMinimum + controllerAimLength)
-		if cameraStickDir.distance_to(-ent.lookDirection) > 1:
-			ent.lookDirection = ent.lookDirection.lerp(cameraStickDir, 20*delta)
-		elif cameraStickDir.length()>0:
-			ent.lookDirection = cameraStickDir
-			controllerAimLength = 0
+
+		# aim assist
+		# by default, make the look direction what we intend for it to be
+		# based on the lerp math...
+		ent.lookDirection = trueLookDirection
+		
+		# ... otherwise, we need to snap it to the best available target.
+		# things like cursor aim still need to be dictated by trueLookPosition
+		
+		if true: # have an "assist enabled for non-mouse aim" settings check here
+			$assist.global_position = $shoulder.global_position
+			if cameraStickDir.length() > 0:
+				$assist.look_at($shoulder.global_position + trueLookDirection);
+			var sucker:Entity = ent.prioritize($assist.get_overlapping_areas())
+			if sucker: ent.lookDirection = ent.shoulderPoint.global_position.direction_to(
+					sucker.shoulderPoint.global_position
+					)
+		
 		lastStickDir = cameraStickDir
-		if Input.get_last_mouse_velocity().length() > 0: mouseLookMode = true
+		
+		if Input.get_last_mouse_velocity().length() > 0:
+			#switch back to mouse aim mode if the mouse cursor moves at all
+			mouseLookMode = true
+			$assist/shape.disabled = true
 		
 	if !Chatbox.isFocused:
 		if Input.is_action_just_pressed("Interact"):
@@ -89,35 +144,26 @@ func _physics_process(delta: float) -> void:
 func _process(delta):
 	if !is_instance_valid(ent): return
 	#region hud stuff
+	
 	ui_hb_over.value = (ent.health / ent.stat_maxHp.val)
 	ui_hb_under.value = lerp(ui_hb_under.value, ui_hb_over.value, delta*7)
 	ui_hb_text.text = str(int(ent.health)) + " / " + str(int(ent.stat_maxHp.val))
 	$shoulder.transform = ent.shoulderPoint.transform
 	if is_instance_valid(Game.effigy):
 		ui_hb_effigy.value = (Game.effigy.health / Game.effigy.stat_maxHp.val)
+	
+	time_elapsed(delta)
+	
 	#endregion
 	
 	#region input stuff
 	if !Chatbox.isFocused:
-		if Input.is_action_just_pressed("PrimaryAttack"):
-			ent.setAbilityPressed.rpc(0, true)
-		if Input.is_action_just_released("PrimaryAttack"):
-			ent.setAbilityPressed.rpc(0, false)
-		if Input.is_action_just_pressed("SecondaryAttack"):
-			ent.setAbilityPressed.rpc(1, true)
-		if Input.is_action_just_released("SecondaryAttack"):
-			ent.setAbilityPressed.rpc(1, false)
-		if Input.is_action_just_pressed("SpecialAbility"):
-			ent.setAbilityPressed.rpc(2, true)
-		if Input.is_action_just_released("SpecialAbility"):
-			ent.setAbilityPressed.rpc(2, false)
-		if Input.is_action_just_pressed("dedicated meow button"):
-			ent.setAbilityPressed.rpc(3, true)
-		if Input.is_action_just_released("dedicated meow button"):
-			ent.setAbilityPressed.rpc(3, false)
+		for i in range(ABILITY_ACTION_MAP.size()):
+			if Input.is_action_just_pressed(ABILITY_ACTION_MAP[i]):
+				ent.setAbilityPressed.rpc(i, true)
+			if Input.is_action_just_released(ABILITY_ACTION_MAP[i]):
+				ent.setAbilityPressed.rpc(i, false)
 	#endregion
-	
-	time_elapsed(delta)
 
 
 # clock controller
